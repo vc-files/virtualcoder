5897dbc445039ea2a3520acb311458aac40271c8
vc-files <80784667+vc-files@users.noreply.github.com>
2021-03-17 00:05:02 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"salom qalesanHey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
